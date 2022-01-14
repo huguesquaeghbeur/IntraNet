c26696698c566d190b92db67f8184c70f632d3d0
christophe991 <christophe.heddebaut@gmail.com>
2022-01-14 14:15:45 +0100

    holiday controller

---
+++ IntraNetBack/IntraNetAPI/Controllers/HolidayController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IntraNetAPI.Controllers
+{
+    public class HolidayController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ IntraNetBack/IntraNetAPI/Repositories/HolidayRepository.cs
@@ -0,0 +1,50 @@
+﻿using IntraNetAPI.Interfaces;
+using IntraNetAPI.Models;
+using IntraNetAPI.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace IntraNetAPI.Repositories
+{
+    public class HolidayRepository : BaseRepository, IRepository<Holiday>
+    {
+        public HolidayRepository(DataContext dataContext) : base(dataContext)
+        {
+        }
+
+        public Holiday FinById(int id)
+        {
+            return _dataContext.Holidays.Find();
+        }
+
+        public IEnumerable<Holiday> GetAll()
+        {
+            return _dataContext.Holidays.ToList();
+        }
+
+        public bool Save(Holiday entity)
+        {
+             _dataContext.Holidays.Add(entity);
+            return _dataContext.SaveChanges() > 0;
+        }
+
+        public IEnumerable<Holiday> Search(Expression<Func<Holiday, bool>> predicate)
+        {
+            return _dataContext.Holidays.ToList();
+        }
+
+        public Holiday SearchOne(Expression<Func<Holiday, bool>> searchMethode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Holiday entity)
+        {
+            _dataContext.Holidays.Update(entity);
+            return _dataContext.SaveChanges() < 0;
+
+        }
+    }
+}
