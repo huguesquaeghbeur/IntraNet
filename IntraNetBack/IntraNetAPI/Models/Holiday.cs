bd7c57274e39b8881da89586b19adea3588da539
vince <vince.leroy@outlook.com>
2022-01-18 14:06:08 +0100

    Commit before Models/Holidays.cs update - for new migration

--- IntraNetBack/IntraNetAPI/Models/Holiday.cs
+++ IntraNetBack/IntraNetAPI/Models/Holiday.cs
@@ -9,12 +9,15 @@ namespace IntraNetAPI.Models
         private DateTime startDate;
         private DateTime endDate;
         private bool startOnMorning;
+        //private bool endOnMorning;
+        //private int halfDayBreakCount;
         public virtual Collaborator Collaborator { get; set; }
         public int Id { get => id; set => id = value; }
         public DateTime StartDate { get => startDate; set => startDate = value; }
         public DateTime EndDate { get => endDate; set => endDate = value; }
         public bool StartOnMorning { get => startOnMorning; set => startOnMorning = value; }
-        
+        //public bool EndOnMorning { get => endOnMorning; set => endOnMorning = value; }
+        //public int HalfDayBreakCount { get => halfDayBreakCount; set => halfDayBreakCount = value; }
         public enum ValidationEnum
         {
             Refused,
--- IntraNetBack/IntraNetAPI/Startup.cs
+++ IntraNetBack/IntraNetAPI/Startup.cs
@@ -33,7 +33,7 @@ namespace IntraNetAPI
             {
                 options.AddPolicy("allConnections", buider =>
                 {
-                    buider.AllowAnyOrigin().AllowAnyMethod();
+                    buider.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
                 });
                 options.AddPolicy("specialOrigin", builder =>
                 {
