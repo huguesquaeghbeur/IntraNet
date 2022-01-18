44ee56b2faac58611250356c83979f774acb760c
flo <dhflo59@gmail.com>
2022-01-18 13:46:08 +0100

    modif mineures startup_cs collabo_controller

--- IntraNetBack/IntraNetAPI/Controllers/CollaboratorController.cs
+++ IntraNetBack/IntraNetAPI/Controllers/CollaboratorController.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Cors;
 
 namespace IntraNetAPI.Controllers
 {
-    [EnableCors("specialOrigin")]
+    [EnableCors("allConnections")]
     [Route("collaborator")]
     public class CollaboratorController : Controller
     {
