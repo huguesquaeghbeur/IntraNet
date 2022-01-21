f7e81033c3a406a2536a939e81830b90e75bec55
flo <dhflo59@gmail.com>
2022-01-21 16:26:32 +0100

    resolve appJS*

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,24 +8,27 @@ import './index.css';
 
 import BillsOverview from "./containers/BillsOverview";
 import AddCollaborator from "./components/AddCollaboratorForm";
-
+import PostRequestHoliday from "./components/holidayComponents/PostRequestHoliday";
+import HolidayList from "./components/holidayComponents/HolidayList";
+import CollaboratorList from "./components/CollaboratorList";
+import CollaboratorManagement from "./components/CollaboratorManagement";
+import CollaboratorPage from "./components/CollaboratorPage";
+import BillById from "./containers/BillById"
+import HolidayById from "./containers/holidayContainers/HolidayById";
+import HolidayMenu from "./containers/holidayContainers/HolidayMenu";
 import DepartmentList from "./components/DptNewFolder/DepartmentList";
 import PostRequestDepartment from "./components/DptNewFolder/PostRequestDepartment";
 import DepartmentById from "./components/DptNewFolder/DepartmentById";
 import { DepartmentsOverview } from "./containers/DepartmentContainer/DepartmentsOverview";
 import HeaderBanner from "./components/baseHeaderFooterEtc/Header";
-// import './index.css';
-
+import DepartmentDescriptionUniqueId from "./components/DptNewFolder/DepartmentDescriptionUniqueId";
 import InfoOverview from "./containers/InfoOverview";
 import InfoList from "./components/InfoList";
-import DepartmentDescriptionUniqueId from "./components/DptNewFolder/DepartmentDescriptionUniqueId";
-import BigCalendar from 'react-big-calendar';
-import Year from './Year';
+// import BigCalendar from 'react-big-calendar';
+// import Year from './Year';
 
-const localizer = BigCalendar.momentLocalizer(moment)
-localizer.formats.yearHeaderFormats = 'YYYY'
-
-
+// const localizer = BigCalendar.momentLocalizer(moment)
+// localizer.formats.yearHeaderFormats = 'YYYY'
 
 
 function App() {
@@ -41,6 +44,26 @@ function App() {
                         element={<BillsOverview />}
                     />
                     <Route
+                        path="bills/:id"
+                        element={<BillById />}/>
+                    <Route
+                        path="holiday"
+                        element={<HolidayMenu />}
+                    />
+                    <Route
+                        path="holiday/list"
+                        element={<HolidayList />}
+                    />
+                    <Route
+                        path="holiday/:id"
+                        element={<HolidayById />}
+                    />
+                    <Route
+                        path="holiday/post"
+                        element={<PostRequestHoliday />}
+                        
+                    />
+                    <Route
                         path="infos"
                         element={<InfoOverview />}
                     />
@@ -50,9 +73,21 @@ function App() {
                     />
                     <Route
                         path="collaborator"
+                        element={<CollaboratorPage />}
+                    />
+                    <Route
+                        path="collaborator/post"
                         element={<AddCollaborator/>}
                     />
                     <Route
+                        path="collaborator/get"
+                        element={<CollaboratorList/>}
+                    />
+                    <Route
+                        path="collaborator/management/:id"
+                        element={<CollaboratorManagement/>}
+                    />
+                        <Route
                         path="departments"
                         element={<DepartmentsOverview/>}
                     />
@@ -71,10 +106,10 @@ function App() {
                     <Route
                         path="departments/description"
                         element={<DepartmentDescriptionUniqueId/>}
-                    />                                  
+                    />                          
                 </Routes>
             </Router>
-            <BigCalendar
+            {/* <BigCalendar
             localizer={localizer}
             events={[]}
             toolbar={true}
@@ -85,12 +120,12 @@ function App() {
                 year: Year
             }}
             messages={{year: 'Year'}}
-            />
+            /> */}
         </div>
     );
 }
 
-const rootElement = document.getElementById('root')
-ReactDOM.render(<App/>, rootElement)
+// const rootElement = document.getElementById('root')
+// ReactDOM.render(<App/>, rootElement)
 
 export default App;
