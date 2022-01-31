681bd51b633e151425e8005363f74843b60a593c
Adrien Masselot <adrien.masselot@gmail.com>
2022-01-31 16:20:58 +0100

    add routing to management button

--- front-end/src/components/baseHeaderFooterEtc/Header.js
+++ front-end/src/components/baseHeaderFooterEtc/Header.js
@@ -3,15 +3,28 @@ import React, { PureComponent } from 'react';
 import InfinGif from "./gif420.gif"
 import "../baseHeaderFooterEtc/Header.css"
 import { Link } from 'react-router-dom';
-import { logout,getUser } from '../../redux/actions/userAction'
+import { logout, getUser } from '../../redux/actions/userAction'
 import { connect } from 'react-redux'
+import Management from '../ManagementButton'
+import { getRole } from '../../services/userService'
 
 class HeaderBanner extends PureComponent {
     constructor(props) {
         super(props);
-        // this.props.getUser()
+        this.state = {
+        }
     }
-    
+    componentDidMount() {
+        if (localStorage.getItem('token') !== null)
+            this.setState({
+                role: getRole()
+            }, () => {
+                console.log(this.state.role)
+            })
+
+    }
+
+
     handleLogoutClick() {
         console.log("handle click")
         this.props.logout()
@@ -25,7 +38,7 @@ class HeaderBanner extends PureComponent {
                     
                 </div> */}
 
-                <nav className='flex items-center justify-between flex-wrap bg-black p-6'>
+                <nav className='bg-black p-6'>
                     <div className='flex items-center flex-shrink-0 text-white mr-6'>
                         {/* <svg className='fill-current h-8 w-8 mr-2' width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" /></svg> */}
                         {/* <img className='gifClass' src={InfinGif} alt="" /> */}
@@ -33,33 +46,36 @@ class HeaderBanner extends PureComponent {
                     </div>
 
                     {localStorage.getItem('token') !== null ?
-                        <div className='w-full block flex-grow lg:flex lg:items-center lg:w-auto'>
-                            <div className='text-sm lg:flex-grow'>
-                                <Link to="/" className='classLienClickable block mt-4 lg:inline-block lg:mt-0 text-orange-400 hover:text-green-200 mr-4'>HOME</Link>
-                                <br />
-                                <br />
-                                <Link to="/departments" className='classLienClickable block mt-4 lg:inline-block lg:mt-0 text-orange-400 hover:text-green-200 mr-4'>Departments</Link>
-                                <Link to="/collaborator" className='classLienClickable block mt-4 lg:inline-block lg:mt-0 text-orange-400 hover:text-green-200 mr-4'>Collaborators</Link>
-                                <Link to="/bills" className='classLienClickable block mt-4 lg:inline-block lg:mt-0 text-orange-400 hover:text-green-200 mr-4'>Bills</Link>
-                            </div>
-                            <div>
-                                <Link to="/"  >
-                                    <button className='inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0'
-                                        onClick={() => this.handleLogoutClick()}
-                                    >
-                                        déconnexion
-                                    </button>
-                                </Link>
+                        <div>
+                            <div className='w-full block flex-grow lg:flex lg:items-center lg:w-auto'>
+                                <div className='text-sm lg:flex-grow'>
+                                    <Link to="/" className='classLienClickable block mt-4 lg:inline-block lg:mt-0 text-orange-400 hover:text-green-200 mr-4'>HOME</Link>
+                                    <br />
+                                    <br />
+                                    <Link to="/departments" className='classLienClickable block mt-4 lg:inline-block lg:mt-0 text-orange-400 hover:text-green-200 mr-4'>Departments</Link>
+                                    <Link to="/collaborator" className='classLienClickable block mt-4 lg:inline-block lg:mt-0 text-orange-400 hover:text-green-200 mr-4'>Collaborators</Link>
+                                    <Link to="/bills" className='classLienClickable block mt-4 lg:inline-block lg:mt-0 text-orange-400 hover:text-green-200 mr-4'>Bills</Link>
+                                </div>
+                                <div className='flex flex-col'>
+                                    <Link to="/"  >
+                                        <button className='inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0'
+                                            onClick={() => this.handleLogoutClick()}
+                                        >
+                                            déconnexion
+                                        </button>
+                                    </Link>
+                                    {this.state.role !== "basic" && this.state.role !== undefined ? <Management currentUser={this.state.role} /> : null}
+
+                                </div>
                             </div>
                         </div>
-
                         :
                         <div>
 
-                        <Link to="/" className='classLienClickable block mt-4 lg:inline-block lg:mt-0 text-orange-400 hover:text-green-200 mr-4'>HOME</Link>
+                            <Link to="/" className='classLienClickable block mt-4 lg:inline-block lg:mt-0 text-orange-400 hover:text-green-200 mr-4'>HOME</Link>
                         </div>
-                    }
 
+                    }
                 </nav>
             </div>
         );
--- front-end/src/services/userService.js
+++ front-end/src/services/userService.js
@@ -1,5 +1,6 @@
 import jwt_decode from "jwt-decode";
-const baseUrl="http://schemas.xmlsoap.org/ws/2005/05/identity/claims/"
+export const baseUrl="http://schemas.xmlsoap.org/ws/2005/05/identity/claims/"
+export const roleUrl = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role"
 
 
 export const tokenToFormData = () => {
@@ -10,4 +11,12 @@ export const tokenToFormData = () => {
     formData.append("email", decoded[baseUrl+"emailaddress"])
     formData.append("password", decoded[baseUrl+"authentication"])
     return formData
+}
+
+export const getRole = () => {
+    const token = localStorage.getItem('token')
+    console.log(token)
+    const decoded = jwt_decode(token)
+    console.log(decoded[roleUrl])
+    return decoded[roleUrl]
 }
